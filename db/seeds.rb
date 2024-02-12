db37e98316da11d23ef613ccaca7c857be9cee76
froma1216 <shoudaisatoh@gmail.com>
2024-02-12 20:47:00 +0900

    seedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -99,3 +99,78 @@ s=Slot.create(
   start_time:'2020-08-15 15:00:00',
   end_time:'2020-08-15 17:00:00')
 t2.slots << s
+
+# パワプロ：投手能力
+pawapuro_pitcher = PawapuroPitcher.create(
+  pace: 163,
+  control: 58,
+  stamina: 80,
+  fastball_type: "ストレート",
+  slider_type_pitch: "スライダー",
+  slider_type_movement: 5,
+  second_slider_type_pitch: "カットボール",
+  second_slider_type_movement: 2,
+  curveball_type_pitch: "スローカーブ",
+  curveball_type_movement: 1,
+  sinker_type_pitch: "Hシンカー",
+  sinker_type_movement: 3,
+  forkball_type_pitch: "フォーク",
+  forkball_type_movement: 4,
+  taipinch: 2,
+  taihidaridasya: -2,
+  utarezuyosa: 0,
+  nobi: -1,
+  quick: 1,
+  other_special_abilities: "キレ○,奪三振,球速安定,投打躍動",
+  created_by: "master",
+  updated_by: "master"
+)
+
+# パワプロ：野手能力
+pawapuro_fielder = PawapuroFielder.create(
+  trajectory: 4,
+  meat: 57,
+  power: 90,
+  running: 78,
+  arm_strength: 85,
+  defense: 42,
+  catching: 52,
+  chance: 2,
+  taihidaritousyu: 0,
+  catcher: 0,
+  tourui: -1,
+  sourui: 2,
+  soukyuu: 1,
+  other_special_abilities: "パワーヒッター,初球○,満塁男,レーザービーム,三振",
+  created_by: "master",
+  updated_by: "master"
+)
+
+# パワプロ：選手情報
+PawapuroPlayer.create(
+  last_name: "大谷",
+  first_name: "翔平",
+  player_name: "大谷翔平",
+  back_name: "OTANI",
+  birthday: Date.new(1993, 7, 15),
+  main_position: 11,
+  p11_proper: 3,
+  p12_proper: 0,
+  p13_proper: 0,
+  p2_proper: 0,
+  p3_proper: 0,
+  p4_proper: 0,
+  p5_proper: 0,
+  p6_proper: 0,
+  p7_proper: 1,
+  throws: "右",
+  bats: "左",
+  pawapuro_pitcher: pawapuro_pitcher,
+  pawapuro_fielder: pawapuro_fielder,
+  kaifuku: 3,
+  kegasinikusa: 2,
+  other_special_abilities: "人気者,強振多用,積極打法,積極走塁",
+  note: "めっちゃ野球うまい。",
+  created_by: "master",
+  updated_by: "master"
+)

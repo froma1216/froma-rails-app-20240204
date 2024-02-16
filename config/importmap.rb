a625012f4ed8c7002f6a43d960b273a98946f90c
froma1216 <shoudaisatoh@gmail.com>
2024-02-16 20:54:31 +0900

    Font Awesome（アイコン）の導入

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap" # @5.3.2
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.5.1/js/all.js"

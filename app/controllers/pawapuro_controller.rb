3dce0c86b6956ea945c18a1dfb7689e610b2f33c
froma1216 <shoudaisatoh@gmail.com>
2024-02-24 21:39:57 +0900

    update時にも登録できるように

--- app/controllers/pawapuro_controller.rb
+++ app/controllers/pawapuro_controller.rb
@@ -41,7 +41,6 @@ class PawapuroController < ApplicationController
 
     # ログインユーザを created_by に設定
     @player.created_by = current_user&.username || ""
-    # FIXME: pawapuro_pitcher,pawapuro_fielderに反映されていない
     @player.pawapuro_pitcher.created_by = current_user&.username || "" if @player.pawapuro_pitcher.present?
     @player.pawapuro_fielder.created_by = current_user&.username || "" if @player.pawapuro_fielder.present?
 
@@ -61,13 +60,31 @@ class PawapuroController < ApplicationController
   # 選手更新アクション
   def update
     @player = PawapuroPlayer.find(params[:id])
+
+    # 選手特殊能力のチェックを連結して格納
+    if params[:pawapuro_player].present? && params[:pawapuro_player][:other_special_abilities].present?
+      player_other_special_abilities = params[:pawapuro_player][:other_special_abilities].join(",")
+      @player[:other_special_abilities] = player_other_special_abilities
+    end
+
+    # 野手特殊能力のチェックを連結して格納
+    if params[:pawapuro_player][:pawapuro_pitcher_attributes].present? && params[:pawapuro_player][:pawapuro_pitcher_attributes][:other_special_abilities].present?
+      pitcher_other_special_abilities = params[:pawapuro_player][:pawapuro_pitcher_attributes][:other_special_abilities].join(",")
+      @player.pawapuro_pitcher[:other_special_abilities] = pitcher_other_special_abilities
+    end
+
+    # 野手特殊能力のチェックを連結して格納
+    if params[:pawapuro_player][:pawapuro_fielder_attributes].present? && params[:pawapuro_player][:pawapuro_fielder_attributes][:other_special_abilities].present?
+      fielder_other_special_abilities = params[:pawapuro_player][:pawapuro_fielder_attributes][:other_special_abilities].join(",")
+      @player.pawapuro_fielder[:other_special_abilities] = fielder_other_special_abilities
+    end
+
     # ログインユーザを updated_by に設定
     @player.updated_by = current_user&.username || ""
-    # FIXME: pawapuro_pitcher,pawapuro_fielderに反映されていない
     @player.pawapuro_pitcher.updated_by = current_user&.username
     @player.pawapuro_fielder.updated_by = current_user&.username
 
-    if @player.update(player_params)
+    if @player.save
       redirect_to pawapuro_index_path, notice: "「選手名：#{@player.player_name}」が更新されました"
     else
       render :edit
@@ -85,9 +102,9 @@ class PawapuroController < ApplicationController
   # 選手作成時のパラメータ
   def player_params
     params.require(:pawapuro_player).permit(
-      :last_name, :first_name, :player_name, :back_name, :birthday, :main_position, :p11_proper, :p12_proper, :p13_proper, :p2_proper, :p3_proper, :p4_proper, :p5_proper, :p6_proper, :p7_proper, :throws, :bats, :kaifuku, :kegasinikusa, :other_special_abilities, :note, :created_by, :updated_by,
-      pawapuro_pitcher_attributes: [:pace, :control, :stamina, :fastball_type, :second_fastball_type, :slider_type_pitch, :slider_type_movement, :second_slider_type_pitch, :second_slider_type_movement, :curveball_type_pitch, :curveball_type_movement, :second_curveball_type_pitch, :second_curveball_type_movement, :shootball_type_pitch, :shootball_type_movement, :second_shootball_type_pitch, :second_shootball_type_movement, :sinker_type_pitch, :sinker_type_movement, :second_sinker_type_pitch, :second_sinker_type_movement, :forkball_type_pitch, :forkball_type_movement, :second_forkball_type_pitch, :second_forkball_type_movement, :original_pitch, :taipinch, :taihidaridasya, :utarezuyosa, :nobi, :quick, :other_special_abilities, :created_by, :updated_by],
-      pawapuro_fielder_attributes: [:trajectory, :meat, :power, :running, :arm_strength, :defense, :catching, :chance, :taihidaritousyu, :catcher, :tourui, :sourui, :soukyuu, :other_special_abilities, :created_by, :updated_by]
+      :last_name, :first_name, :player_name, :back_name, :birthday, :main_position, :p11_proper, :p12_proper, :p13_proper, :p2_proper, :p3_proper, :p4_proper, :p5_proper, :p6_proper, :p7_proper, :throws, :bats, :kaifuku, :kegasinikusa, :note, :created_by, :updated_by, :other_special_abilities,
+      pawapuro_pitcher_attributes: [:id, :pace, :control, :stamina, :fastball_type, :second_fastball_type, :slider_type_pitch, :slider_type_movement, :second_slider_type_pitch, :second_slider_type_movement, :curveball_type_pitch, :curveball_type_movement, :second_curveball_type_pitch, :second_curveball_type_movement, :shootball_type_pitch, :shootball_type_movement, :second_shootball_type_pitch, :second_shootball_type_movement, :sinker_type_pitch, :sinker_type_movement, :second_sinker_type_pitch, :second_sinker_type_movement, :forkball_type_pitch, :forkball_type_movement, :second_forkball_type_pitch, :second_forkball_type_movement, :original_pitch, :taipinch, :taihidaridasya, :utarezuyosa, :nobi, :quick, :other_special_abilities, :created_by, :updated_by],
+      pawapuro_fielder_attributes: [:id, :trajectory, :meat, :power, :running, :arm_strength, :defense, :catching, :chance, :taihidaritousyu, :catcher, :tourui, :sourui, :soukyuu, :other_special_abilities, :created_by, :updated_by]
     )
   end
 end

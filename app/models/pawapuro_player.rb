0c374cc4af010df9c1ecf855fbc21d1e0d63aba8
froma1216 <shoudaisatoh@gmail.com>
2024-03-01 21:00:42 +0900

    パワプロ：必須項目に「必須」バッチを設定

--- app/models/pawapuro_player.rb
+++ app/models/pawapuro_player.rb
@@ -2,6 +2,11 @@ class PawapuroPlayer < ApplicationRecord
   has_one :pawapuro_pitcher, dependent: :destroy
   has_one :pawapuro_fielder, dependent: :destroy
 
+  validates :last_name, { presence: true }
+  validates :first_name, { presence: true }
+  validates :player_name, { presence: true }
+  validates :main_position, { presence: true }
+
   # ネストされた属性の許可
   accepts_nested_attributes_for :pawapuro_pitcher, :pawapuro_fielder
 end

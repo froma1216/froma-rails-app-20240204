966b66901361a55770d45ecd564bd11f17d1764e
froma1216 <shoudaisatoh@gmail.com>
2024-03-16 15:56:59 +0900

    ポケモン：ページネーション時にNoを通番表示

--- app/controllers/pokemon_emerald_factory_participants_controller.rb
+++ app/controllers/pokemon_emerald_factory_participants_controller.rb
@@ -9,6 +9,12 @@ class PokemonEmeraldFactoryParticipantsController < ApplicationController
     @pokemon_names = PokemonEmeraldFactoryParticipant.pluck(:name).uniq
 
     @results = Kaminari.paginate_array(@results).page(params[:page])
+    # 通番の計算用に現在のページ番号を取得（指定がない場合は1とする）
+    current_page = params[:page] ? params[:page].to_i : 1
+    # ページごとの表示件数
+    per_page = @results.limit_value
+    # ビューで使用するための基点となる通番を計算
+    @starting_number = (current_page - 1) * per_page
 
     # 検索結果が空の場合にフラッシュメッセージを設定
     flash.now[:alert] = "対象のポケモンが見つかりませんでした。" if @results.empty?

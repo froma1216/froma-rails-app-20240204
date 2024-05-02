e1b4dbd2044f9156c62b83875ef968b9fc9661e1
froma1216 <shoudaisatoh@gmail.com>
2024-05-02 14:19:05 +0900

    エメラルド：初期表示は一覧表示しない＋周検索追加

--- app/controllers/pokemon_emerald_factory_participants_controller.rb
+++ app/controllers/pokemon_emerald_factory_participants_controller.rb
@@ -3,21 +3,26 @@ class PokemonEmeraldFactoryParticipantsController < ApplicationController
 
   # 一覧画面
   def index
-    # 検索結果
-    @results = @q.result
+    if params[:q].present?
+      # 検索結果
+      # @results = @q.result
+      @results = Kaminari.paginate_array(@results).page(params[:page])
+
+      # 通番の計算用に現在のページ番号を取得（指定がない場合は1とする）
+      current_page = params[:page] ? params[:page].to_i : 1
+      # ページごとの表示件数
+      per_page = @results.limit_value
+      # ビューで使用するための基点となる通番を計算
+      @starting_number = (current_page - 1) * per_page
+
+      # 検索結果が空の場合にフラッシュメッセージを設定
+      flash.now[:alert] = "対象のポケモンが見つかりませんでした。" if @results.empty?
+    else
+      # 初期状態で何も表示しない
+      @results = PokemonEmeraldFactoryParticipant.none
+    end
     # PokemonEmeraldFactoryParticipantモデルからポケモンの名前を全て取得
     @pokemon_names = PokemonEmeraldFactoryParticipant.pluck(:name).uniq
-
-    @results = Kaminari.paginate_array(@results).page(params[:page])
-    # 通番の計算用に現在のページ番号を取得（指定がない場合は1とする）
-    current_page = params[:page] ? params[:page].to_i : 1
-    # ページごとの表示件数
-    per_page = @results.limit_value
-    # ビューで使用するための基点となる通番を計算
-    @starting_number = (current_page - 1) * per_page
-
-    # 検索結果が空の場合にフラッシュメッセージを設定
-    flash.now[:alert] = "対象のポケモンが見つかりませんでした。" if @results.empty?
   end
 
   private
@@ -26,5 +31,7 @@ class PokemonEmeraldFactoryParticipantsController < ApplicationController
   # NOTE: https://plog.kobacchi.com/rails7-ransack-instant-search/
   def set_q
     @q = PokemonEmeraldFactoryParticipant.ransack(params[:q])
+    # @q.result = @q.result.with_lap_show(params[:lap_number]) if params[:lap_number].present?
+    @results = params[:lap_number].present? ? @q.result.with_lap_show(params[:lap_number]) : @q.result
   end
 end
--- app/models/pokemon_emerald_factory_participant.rb
+++ app/models/pokemon_emerald_factory_participant.rb
@@ -1,10 +1,16 @@
 class PokemonEmeraldFactoryParticipant < ApplicationRecord
-  # scope :with_lap, lambda { |lap_number|
-  #   where("lap#{lap_number}_show = ?", 1)
-  # }
-
   def self.ransackable_attributes(_auth_object = nil)
     # 検索条件を追加
-    %w[name]
+    %w[name lap1_show lap2_show lap3_show lap4_show lap5_show lap6_show lap7_show lap8_show]
+  end
+
+  # 周ごとのフィルタをカスタムスコープとして追加
+  def self.ransackable_scopes(_auth_object = nil)
+    %i[with_lap_show]
+  end
+
+  # 周ごとのカラムの値が1のレコードをフィルタするスコープ
+  def self.with_lap_show(lap_number)
+    where("lap#{lap_number}_show = ?", 1)
   end
 end

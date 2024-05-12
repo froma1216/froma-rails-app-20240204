144a3bafe8d2cd15da22ed7850622c1747781fe0
froma1216 <shoudaisatoh@gmail.com>
2024-05-12 16:10:17 +0900

    エメラルド：検索ポケモンのタイプを表示

--- app/models/pokemon_emerald_factory_participant.rb
+++ app/models/pokemon_emerald_factory_participant.rb
@@ -1,4 +1,10 @@
 class PokemonEmeraldFactoryParticipant < ApplicationRecord
+  # 指定された名前のポケモンのタイプを返すクラスメソッド
+  def self.get_types_by_name(name)
+    pokemon = find_by(name:)
+    [pokemon&.type1, pokemon&.type2]
+  end
+
   def self.ransackable_attributes(_auth_object = nil)
     # 検索条件を追加
     %w[name lap1_show lap2_show lap3_show lap4_show lap5_show lap6_show lap7_show lap8_show]

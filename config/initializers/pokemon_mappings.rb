9b3eb3cc325a71c618d4d7042b079691486e92ca
froma1216 <shoudaisatoh@gmail.com>
2024-05-12 17:42:24 +0900

    エメラルド：道具画像を表示

--- config/initializers/pokemon_mappings.rb
+++ config/initializers/pokemon_mappings.rb
@@ -1,2 +1,3 @@
 POKEMON_NAME_MAPPINGS = YAML.load_file(Rails.root.join("config/pokemon_emerald_factory_participant/name_mappings.yml"))["names"]
 POKEMON_TYPE_MAPPINGS = YAML.load_file(Rails.root.join("config/pokemon_emerald_factory_participant/type_mappings.yml"))["types"]
+POKEMON_ITEM_MAPPINGS = YAML.load_file(Rails.root.join("config/pokemon_emerald_factory_participant/item_mappings.yml"))["items"]

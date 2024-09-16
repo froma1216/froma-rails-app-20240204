e5ee8946ad2d5a941a47a73d4b8b0b5f7ceaaeb4
froma1216 <shoudaisatoh@gmail.com>
2024-09-16 15:56:09 +0900

    mhxx：更新処理＋コントローラーリファクタリング

--- app/controllers/mhxx/times_controller.rb
+++ app/controllers/mhxx/times_controller.rb
@@ -1,73 +1,46 @@
 class Mhxx::TimesController < ApplicationController
-  # TODO: editではなくcreate?パワプロも
-  before_action :ensure_currect_user, { only: [:edit, :update, :destroy] }
+  before_action :ensure_currect_user, only: [:edit, :update, :destroy]
+  before_action :set_weapon_and_hunter_art_data, only: [:new, :edit, :create, :update]
 
   def new
     if current_user.present?
-      # クエスト情報の取得
       @quest = Mhxx::MQuest.find(params[:quest_id])
-      # 新しいタイムの初期化
       @time = Mhxx::Time.new
-      weapon_types = Mhxx::MWeaponType.all
-      # 武器種のセレクトボックス用データ
-      @grouped_weapon_types = Enums.weapon_type_division.each_with_object({}) do |(key, division), grouped|
-        next if division == 200 # 「共通」は除く
-        grouped[I18n.t("enums.mhxx.weapon_type_division.#{key}")] = weapon_types.where(weapon_type_division: division).pluck(:name, :id)
-      end
-      # 狩技のセレクトボックス用データ
-      common_weapon_types, other_weapon_types = weapon_types.partition { |wt| wt.weapon_type_division == 200 }
-      @grouped_hunter_arts = (common_weapon_types + other_weapon_types).map do |weapon_type|
-        [
-          weapon_type.name, # グループ名（武器タイプ名）
-          weapon_type.m_hunter_art.map { |art| [art.name, art.id] } # 各グループのオプション（ハンターアーツ名とID）
-        ]
-      end
-      # 初期表示用の「選択なし」を追加
-      @grouped_hunter_arts.unshift(["なし", [["なし", ""]]])
     else
       redirect_to new_user_session_path
     end
   end
 
   def edit
+    @time = Mhxx::Time.find(params[:id])
+    @quest = @time.m_quest # 既存のタイムから関連するクエストを取得
   end
 
   def create
-    # クエスト情報の取得
+    @quest = Mhxx::MQuest.find(params[:mhxx_time][:m_quest_id]) # フォームから渡されたクエストIDを利用
     @time = Mhxx::Time.new(time_params)
     @time.user = current_user
-    @quest = Mhxx::MQuest.find(params[:quest_id])
-
-    # 武器種のセレクトボックス用データ
-    weapon_types = Mhxx::MWeaponType.all
-    @grouped_weapon_types = Enums.weapon_type_division.each_with_object({}) do |(key, division), grouped|
-      next if division == 200 # 「共通」は除く
-      grouped[I18n.t("enums.mhxx.weapon_type_division.#{key}")] = weapon_types.where(weapon_type_division: division).pluck(:name, :id)
-    end
-
-    # 狩技のセレクトボックス用データ
-    common_weapon_types, other_weapon_types = weapon_types.partition { |wt| wt.weapon_type_division == 200 }
-    @grouped_hunter_arts = (common_weapon_types + other_weapon_types).map do |weapon_type|
-      [
-        weapon_type.name, # グループ名（武器タイプ名）
-        weapon_type.m_hunter_art.map { |art| [art.name, art.id] } # 各グループのオプション（ハンターアーツ名とID）
-      ]
-    end
 
     if @time.save
-      # 成功時の処理（例：リダイレクト）
       redirect_to mhxx_quests_path, notice: "タイムが作成されました"
     else
-      # 失敗時の処理
-      Rails.logger.info @time.errors.full_messages.inspect
       render :new
     end
   end
 
   def update
+    @time = Mhxx::Time.find(params[:id])
+    @quest = @time.m_quest # 更新対象のタイムからクエストを取得
+
+    if @time.update(time_params)
+      redirect_to mhxx_quests_path, notice: "タイムが更新されました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @time = Mhxx::Time.find(params[:id])
     @time.destroy
     redirect_to mhxx_quests_path, notice: "タイムが削除されました"
   end
@@ -86,5 +59,24 @@ class Mhxx::TimesController < ApplicationController
   def ensure_currect_user
     @time = Mhxx::Time.find(params[:id])
     redirect_to mhxx_quests_path, notice: "権限がありません" if @time.user != current_user
+  end
+
+  # 武器種と狩技のデータを取得
+  def set_weapon_and_hunter_art_data
+    weapon_types = Mhxx::MWeaponType.all
+
+    @grouped_weapon_types = Enums.weapon_type_division.each_with_object({}) do |(key, division), grouped|
+      next if division == 200 # 「共通」は除く
+      grouped[I18n.t("enums.mhxx.weapon_type_division.#{key}")] = weapon_types.where(weapon_type_division: division).pluck(:name, :id)
+    end
+
+    common_weapon_types, other_weapon_types = weapon_types.partition { |wt| wt.weapon_type_division == 200 }
+    @grouped_hunter_arts = (common_weapon_types + other_weapon_types).map do |weapon_type|
+      [
+        weapon_type.name, # グループ名（武器タイプ名）
+        weapon_type.m_hunter_art.map { |art| [art.name, art.id] } # 各グループのオプション（狩技名とID）
+      ]
+    end
+    @grouped_hunter_arts.unshift(["なし", [["なし", ""]]])
   end
 end

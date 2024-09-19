6396a305309c62acdbae70feca678bc5aaf89084
froma1216 <shoudaisatoh@gmail.com>
2024-09-19 16:55:50 +0900

    mhxx：スキル登録レイアウト

--- app/models/enums.rb
+++ app/models/enums.rb
@@ -14,6 +14,11 @@ class Enums
     def element
       YAML.load_file(Rails.root.join("config", "mhxx", "element.yml"))['element']
     end
+
+    # スキル区分
+    def skill_division
+      YAML.load_file(Rails.root.join("config", "mhxx", "skill_division.yml"))['skill_division']
+    end
     # 以下に追加
   end
 end

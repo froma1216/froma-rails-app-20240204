b45c39653bde351ba53b935112c00d36a82952f3
shoudaisatoh <shoudaisatoh@gmail.com>
2024-10-06 23:05:52 +0900

    MHXX：クエスト説明文の追加

--- app/helpers/mhxx/quests_helper.rb
+++ app/helpers/mhxx/quests_helper.rb
@@ -16,4 +16,73 @@ module Mhxx::QuestsHelper
     fraction = ((remaining_seconds - seconds) * 100).round
     format("%<minutes>02d'%<seconds>02d\"%<fraction>02d", minutes:, seconds:, fraction:)
   end
+
+  # クエスト説明文
+  def quest_main_target(quest, monsters)
+    case quest.quest_division
+    when "gathering"
+      # 採取
+      # 「急募・単行採掘求ム」のみ
+      "燃石炭１０個以上納品<br>もしくはネコタクチケットの納品".html_safe
+    when "hunt"
+      # 狩猟
+      generate_hunt_target(monsters)
+    when "capture"
+      # 捕獲
+      "#{monsters[0].name}１頭の捕獲"
+    when "multi_monster"
+      # 大連続
+      "全ての大型モンスターの狩猟"
+    when "slay"
+      # 討伐
+      generate_slay_target(monsters)
+    when "special"
+      # 特殊
+      "#{monsters[0].name}の討伐"
+    when "special_permit"
+      # 特殊許可
+      generate_special_permit_target(quest, monsters)
+    # when "prowler"
+    #   # ニャンター
+    #   "猫です"
+    else
+      "クエスト区分が存在しません。"
+    end
+  end
+
+  private
+
+  # 狩猟の詳細分岐
+  def generate_hunt_target(monsters)
+    if monsters[1].nil?
+      "#{monsters[0].name}１頭の狩猟"
+    elsif monsters[0].id == monsters[1].id
+      "#{monsters[0].name}２頭の狩猟"
+    else
+      "#{monsters[0].name}１頭と<br>#{monsters[1].name}１頭の狩猟".html_safe
+    end
+  end
+
+  # 討伐の詳細分岐
+  def generate_slay_target(monsters)
+    elder_dragon_class_ids = [72, 75, 76] + (81..93).to_a
+    if elder_dragon_class_ids.include?(monsters[0].id)
+      "#{monsters[0].name}の討伐"
+    else
+      "#{monsters[0].name}１頭の討伐"
+    end
+  end
+
+  # 特殊許可の詳細分岐
+  def generate_special_permit_target(quest, monsters)
+    if quest.name.include?("捕獲")
+      "#{monsters[0].name}１頭の捕獲"
+    elsif monsters[2].present?
+      "全ての大型モンスターの狩猟"
+    elsif monsters[0].id == monsters[1].id
+      "#{monsters[0].name}連続２頭の狩猟"
+    else
+      generate_hunt_target(monsters)
+    end
+  end
 end

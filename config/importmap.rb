ca84c67ef060bd92d6a4524ed649d46e5ca71dde
shoudaisatoh <shoudaisatoh@gmail.com>
2024-10-18 21:11:41 +0900

    全体：Importmapの微修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,10 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Bootstrap and Popper.js
 pin "bootstrap" # @5.3.2
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+
+# Font Awesome
 pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.5.1/js/all.js"
